4fa5f47c7f49221cd5d8064650492474a6344435
Oleg Mykhalyna <oleg.muhaluna@gmail.com>
2019-11-21 21:49:26 +0200

    add stream

--- HM-1/task-2.js
+++ HM-1/task-2.js
@@ -1,25 +1,8 @@
-const csv = require('csvtojson');
-const fs = require("fs");
+const csv = require("csvtojson");
+const { createReadStream, createWriteStream } = require("fs");
+const csvFilePath = "./csv/node_mentoring_t1_2_input_example.csv";
 
-const csvFilePath= './csv/node_mentoring_t1_2_input_example.csv';
+const readStream = createReadStream(csvFilePath);
+const writeStream = createWriteStream("./books.txt");
 
-const convertToJson =  async (path) => {
-	const convertedJson = await csv().fromFile(path);
-	convertedJson.map(book => {
-		const mappedBook = {book: book.Book, author: book.Author, price: book.Price};
-		appendBook(mappedBook);
-		return mappedBook;
-	});
-};
-
-const appendBook = (book) => {
-	fs.appendFile('books.txt', JSON.stringify(book) + '\n', (err) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log('Saved!');
-	});
-};
-
-convertToJson(csvFilePath);
+readStream.pipe(csv()).pipe(writeStream);
--- HM-1/task1.js
+++ HM-1/task1.js
@@ -1,11 +1,15 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
 	input: process.stdin,
-	output: null,
+	output: null
 });
 
-
-rl.on('line', function(line){
-	console.log(line.split('').reverse().join(''));
+rl.on("line", function(line) {
+	console.log(
+		line
+			.split("")
+			.reverse()
+			.join("")
+	);
 });
