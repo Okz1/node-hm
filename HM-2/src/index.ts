6e29f924d22eac7e1b5d904533a3b6c90871b521
Oleg Mykhalyna <oleg.muhaluna@gmail.com>
2019-12-15 21:17:05 +0200

    HW-2

--- HM-2/src/index.ts
+++ HM-2/src/index.ts
@@ -1,8 +1,9 @@
 import express, { Application } from 'express';
-import { userRouter } from "./resourses/users/user-router";
+import { userRouter } from './resourses/users/user-router';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use('/', userRouter);
+
 app.listen(5000, () => console.log('Server running'));
